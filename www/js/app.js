b27c0a4062d443e759311050b3dc2a2c74979ae9
eatsleepvector <eatsleepvector@gmail.com>
2016-07-20 23:12:03 -0400

    scrollReveal

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+var coverReveal = {
+	  delay    : 200,
+	  distance : '90px',
+	  easing   : 'ease-in-out',
+	  rotate   : { z: 10 },
+	  scale    : 1.1
+	};
+window.sr = ScrollReveal();
+sr.reveal('.project-cover', coverReveal);
